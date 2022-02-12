02faf34cb13398943dc71e64d7c5a395593f5876
[Lerts] <[oddff@mail.ru]>
2022-02-12 16:33:42 +0300

    Были созданы объекты астероидов

---
+++ AsteroidGame/BaseObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace AsteroidGame
+{
+    internal class BaseObject
+    {
+        protected Point _Pos;
+        protected Point _Dir;
+        protected Size _Size;
+
+        public BaseObject(Point pos, Point dir, Size size)
+        {
+            _Pos = pos;
+            _Dir = dir;
+            _Size = size;
+        }
+
+        public void Draw()
+        {
+            Game.__buffer.Graphics.DrawEllipse(Pens.White, _Pos.X, _Pos.Y, _Size.Width, _Size.Height);
+        }
+
+        public void Update()
+        {
+            _Pos.X += _Dir.X;
+            _Pos.Y += _Dir.Y;
+            if(_Pos.X < 0) _Dir.X = -_Dir.X;
+            if ((_Pos.X) > Game.__Width-20) _Dir.X = -_Dir.X;
+
+            if (_Pos.Y < 0) _Dir.Y = -_Dir.Y;
+            if (_Pos.Y > Game.__Height-20) _Dir.Y = -_Dir.Y;
+        }
+
+    }
+}
--- AsteroidGame/Game.cs
+++ AsteroidGame/Game.cs
@@ -12,6 +12,10 @@ namespace AsteroidGame
         public static int __Width { get; set; }
         public static int __Height { get; set; }
 
+        public static BaseObject[] __objs;
+
+        
+
         static Game() { }
 
         public static void Init(Form form)
@@ -26,15 +30,54 @@ namespace AsteroidGame
             __Height = form.ClientSize.Height;
 
             __buffer = __context.Allocate(g, new Rectangle(0, 0, __Width, __Height));
+           
+            Load();
 
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
         }
 
         public static void Draw()
-        {
+        {           
             __buffer.Graphics.Clear(Color.Black);
-            __buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
-            __buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            foreach (BaseObject item in __objs)
+            {
+                item.Draw();
+            }
             __buffer.Render();
         }
+
+        public static void Update()
+        {
+            foreach (BaseObject item in __objs)
+                item.Update();
+        }
+
+        public static void Load()
+        {
+            Random r = new Random();
+            __objs = new BaseObject[30];
+            for (int i = 0; i < __objs.Length; i++)
+            {
+                __objs[i] = new BaseObject(new Point(r.Next(0, __Height), r.Next(0, __Width - 300)), 
+                                            new Point(RAN(), RAN()), 
+                                            new Size(20, 20));
+            }
+
+            int RAN()
+            {
+                int ran = r.Next(-15, 15);
+                if ( ran == 0)
+                    return 1;
+                return ran;
+            }
+        }
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
     }
 }
